a65b184f9ce51ae0d7927d972d7db1956f8d6f0a
IvoPaunov <ivo.paunov@gmail.com>
2015-03-11 02:06:34 +0200

    OOP - Defining Classes - Part 1

---
+++ Modul_1/OOP/01_ Defining_Classes _Part_1/MobileDevices/MobileDevices/Battery.cs
@@ -0,0 +1,81 @@
+﻿namespace MobileDevices
+{
+    using System;    
+
+    public class Battery : Device
+    {
+        // fields
+        private int hoursIdle;
+        private int hoursTalk;
+        private BatteryTypes batteryType;
+
+        // constructors
+        public Battery(string setModel, Manufacturers setManifacturer)
+            : base(setModel, setManifacturer)
+        {
+        }
+
+        public Battery(string setModel, Manufacturers setManifacturer, BatteryTypes setBatteryType)
+            : base(setModel, setManifacturer)
+        {
+            this.batteryType = setBatteryType;
+        }
+
+        public Battery(string setModel, Manufacturers setManifacturer,  BatteryTypes setBatteryType, int setHoursIdle, int setHoursTalk)
+            : this(setModel, setManifacturer, setBatteryType)            
+        {
+            this.HoursIdle = setHoursIdle;
+            this.HoursTalk = setHoursTalk;
+        }
+        
+        // properties
+        public BatteryTypes BatteryType
+        {
+            get 
+            {
+                return this.batteryType;
+            }
+
+            private set
+            {
+                this.batteryType = value;
+            }
+        }
+
+        public int HoursIdle
+        {
+            get
+            {
+                return this.hoursIdle;
+            }
+
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("Batery idle hours cannot be  negative!");
+                }
+
+                this.hoursIdle = value;
+            }
+        }
+
+        public int HoursTalk
+        {
+            get
+            {
+                return this.hoursTalk;
+            }
+
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("Batery talk hours cannot be  negative!");
+                }
+
+                this.hoursTalk = value;
+            }
+        }
+    }
+}
---
+++ Modul_1/OOP/01_ Defining_Classes _Part_1/MobileDevices/MobileDevices/BatteryTypes.cs
@@ -0,0 +1,12 @@
+﻿namespace MobileDevices
+{
+    // enumeration
+    public enum BatteryTypes 
+    {
+        NOT_SET,
+        Li_Ion,
+        Li_Pol,
+        NiMH,
+        NiCd,
+    }
+}
